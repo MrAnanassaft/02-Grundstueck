7acb9ca6c29cfd423adf0741465260d875faca01
jianalex <alex20jiang07@gmail.com>
2022-08-30 11:26:38 +0200

    Grundstück berechnung

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,5 +1,8 @@
 public class MainProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Property p1 = new Property("erzbergerstraße 1", 10, 20 );
+        System.out.println(p1.getAddress() + "-" + p1.getSize() + "m²");
+        Property p2 = new Property("erzbergerstraße 2", 12, 20 );
+        System.out.println(p2.getAddress() + "-" + p2.getSize() + "m²");
     }
 }
