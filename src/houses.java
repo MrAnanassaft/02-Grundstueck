b176f2a0dd96ef370d8f51edd993be993d90835b
jianalex <alex20jiang07@gmail.com>
2022-08-31 15:06:18 +0200

    Haus

--- src/houses.java
+++ src/houses.java
@@ -1,2 +1,15 @@
 public class houses {
+
+    private int length;
+    private int width;
+    private int high;
+
+    public houses(int length, int width, int high){
+        this.length = length;
+        this.width = width;
+        this.high = high;
+    }
+    public getVolume(){
+        return length*width*high
+    }
 }
