fe15a90150dc4e25755c0bf33980265f0b9dab7b
jianalex <alex20jiang07@gmail.com>
2022-09-06 10:49:43 +0200

    Haus

--- src/House.java
+++ src/House.java
@@ -3,13 +3,18 @@ public class House {
     private int length;
     private int width;
     private int high;
+    private String address;
 
-    public House(int length, int width, int high){
+    public House(int length, int width, int high, String address){
         this.length = length;
         this.width = width;
         this.high = high;
+        this.address = address;
     }
     public double getVolume(){
         return length*width*high;
+    }
+    public String getAddress(){
+        return this.address;
     }
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -4,5 +4,7 @@ public class MainProgram {
         System.out.println(p1.getAddress() + "-" + p1.getSize() + "m²");
         Property p2 = new Property("erzbergerstraße 2", 12, 20 );
         System.out.println(p2.getAddress() + "-" + p2.getSize() + "m²");
+        House h1 = new House(80 , 70, 20, "erzbergerstraße 1");
+        System.out.println(h1.getVolume() + "m³  " + h1.getAddress());
     }
 }
